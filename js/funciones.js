3d46fd1d83ea5d18aa034b0320f2556bf0038039
germanCardozoE <german.e.cardozo2020@gmail.com>
2024-05-17 21:23:31 -0300

    validacion del form y seccion destinos

--- js/funciones.js
+++ js/funciones.js
@@ -1,143 +1,82 @@
 /* //////////////////////////////////////////////////////////
-   ////////////   **FUNCION PARA HEADER**   /////////////////
-*/ //////////////////////////////////////////////////////////
-
-function header() {
-    document.write(
-        // La siguiente linea crea un contenedor, lo que permite dar un formato especifico a todo lo interior
-        '<div class="container">',
-        // La siguiente linea hace que al presionar aqui se salte a la pagina del index 
-        '   <a href="../index.html">',
-        // La siguiente linea pone una imagen sobre la cual clickear 
-        '       <img src="img/logo.jpeg" alt="logo turismo argentina">',
-        '   </a>',
-        
-        
-        // La siguiente linea coloca secciones a las cual saltar
-        '   <nav>',
-        '       <ul id="menu">',
-        '           <li><a href="../pages/nosotros.html" >Nosotros</a></li>',
-        '           <li><a href="../pages/destinos.html">DESTINOS</a></li>',
-        '           <li><a href="../pages/register.html">VIAJES COMPARTIDOS</a></li>',
-        '           <li><a href="../pages/ofertar.html">OFRECÉ TU EMPRENDIMIENTO</a></li>',
-        '       </ul>',
-        '       <ul id="ingreso">',
-        '            <li><a href="../pages/login.html">INGRESAR</a></li>',
-        '       <li><a href="../pages/register.html">REGISTARSE</a></li>',
-        '       </ul>',
-        '   </nav>',
-        '   <a href="#" class="hamb" id="hamb"><i class="bi bi-list"></i></a>',
-        '</div>'
-    )
-}
-
-/* //////////////////////////////////////////////////////////
-   ////////////   **FUNCION PARA FOOTER**   /////////////////
-*/ //////////////////////////////////////////////////////////
-function footer() {
-    document.write(
-        '<div class="foot">',
-        '     <div class="foot_img">',
-        '        <img src="imagenes/Logo.png" class="logo-footer">',
-        '       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>',
-        '  </div>',
-        ' <div class="foot_temas">',
-        '    <h3>',
-        '       Temas relacionados',
-        '  </h3>',
-        ' <ul>',
-        '    <li>HTML </a>',
-        '   </li>',
-        '  <li>CSS</a>',
-        ' </li>',
-        '<li>JS</a>',
-        '                </li>',
-        '           </ul>',
-        '      </div>',
-        '     <div class="foot_autores">',
-        '        <h3> Autores: </h3>',
-        '     <ul>',
-        '       <li>Celeste De Micco</li>',
-        '       <li>Germán Cardozo</li>', 
-        ' </ul>',
-        '        </div>',
-        '       <div class="foot_redes">',
-        '          <h3>',
-        '             Redes Sociales',
-        '        </h3>',
-        '       <ul class="redes">',
-        '          <li>',
-        '             <a href="#">',
-        '                <i class="fa-brands fa-facebook"></i>',
-        '           </a>',
-        '      </li>',
-        '     <li>',
-        '        <a href="#">',
-        '           <i class="fa-brands fa-twitter-square"></i>',
-        '      </a>',
-        ' </li>',
-        '<li>',
-        '                    <a href="#">',
-        '                       <i class="fa-brands fa-instagram"></i>',
-        '                  </a>',
-        '             </li>',
-        '        </ul>',
-        '   </div>',
-        '</div>',
-        '<div class="barra-footer">',
-        '    &copy; Comisión 24126 - Grupo 21',
-        '</div>',
-
-    )
-
-}
-
-
-/* //////////////////////////////////////////////////////////
    ///////   **FUNCIONES PARA VALIDAR FORMULARIOS**   ///////
 */ //////////////////////////////////////////////////////////
+
 function validarEnviar() {
-    //valido el nombre
-    if (document.fvalida.nombre.value.length <= 5) {
-        alert("Tiene que escribir su nombre")
-        document.fvalida.nombre.focus()
-        return 0;
-    }
 
-    //valido edad
-    var testear = document.fvalida.edad.checked;
-    if (!testear) {
-        alert('Debe ser mayor de 18 años para contactarse con nosotros');
-        document.fvalida.edad.focus()
-        return 0;
+    var formulario = document.forms["fvalida"];
+  
+    // Valida el nombre
+    if (formulario.nombre.value.length < 5) { // Cambiado de <= a <
+        alert("El nombre debe tener al menos 5 caracteres");
+        formulario.nombre.focus();
+        return false;
     }
-
-    //valido email
-    testear = /^\w+([\.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|es)+$/
-    if (!testear.test(document.fvalida.email.value)) {
-        alert("La dirección de email  no es valida");
-        document.fvalida.email.focus()
-        return 0;
+  
+    // Valida email
+    var testear = /^\w+([\.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|es)+$/;
+    if (!testear.test(formulario.email.value)) {
+        alert("La dirección de email no es válida");
+        formulario.email.focus();
+        return false;
     }
+  
+    // Valida el interés -> no tiene sentido porque ya aparece una opcion por defecto
+    /*if (formulario.intereses.selectedIndex ==='') {
+        alert("Debe seleccionar un interes");
+        formulario.intereses.focus();
+        return false;
+    }*/
+  
+    // Valida el sexo
+  // Valida el campo de sexo
+  var sexoRadios = formulario.sexo;
+  var sexoSeleccionado = false;
+  for (var i = 0; i < sexoRadios.length; i++) {
+      if (sexoRadios[i].checked) {
+          sexoSeleccionado = true;
+          break;
+      }
+  }
+  if (!sexoSeleccionado) {
+      alert("Debe seleccionar una opción de sexo");
+      return false;
+  }
 
-    //valido el interés
-    if (document.fvalida.interes.selectedIndex == 0) {
-        alert("Debe seleccionar un motivo de su contacto.")
-        document.fvalida.interes.focus()
-        return 0;
+  
+    // Valida el campo de hobbies
+    var checkboxesHobbies = formulario.hobbies;
+    var seleccionado = false;
+    for (var i = 0; i < checkboxesHobbies.length; i++) {
+        if (checkboxesHobbies[i].checked) {
+            seleccionado = true;
+            break;
+        }
     }
-
-    //valido el nombre
-    if (document.fvalida.mensaje.length == 0) {
-        alert("Debe dejar un mensaje")
-        document.fvalida.mensaje.focus()
-        return 0;
+    if (!seleccionado) {
+        alert("Debe seleccionar al menos un hobbie");
+        return false;
     }
-
-    //Finalmente, si llegó hasta aqui, se envia el form.
+    
+    // Valida el mensaje
+    if (formulario.comentario.value.length === 0) {
+        alert("Debe dejar un comentario");
+        formulario.comentario.focus();
+        return false;
+    }
+  
+    // Valida si acepta los términos y condiciones
+    if (!formulario.terminos.checked) {
+        alert("Debe aceptar los términos y condiciones");
+        formulario.terminos.focus();
+        return false;
+    }
+  
+    // Si llegó hasta aquí, el formulario es válido
     alert("Muchas gracias por enviar el formulario");
-    document.fvalida.submit();
-}
+    return true;
+  }
+  
 
 /* //////////////////////////////////////////////////////////
    ///////   **FUNCIONES PARA VALIDAR FORMULARIOS**   ///////
